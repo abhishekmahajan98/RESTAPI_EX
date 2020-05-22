5cf72384b046812220eeff4a4abee6efa730a9ca
abhishek <abhishek.mahajan314@gmail.com>
2020-05-23 00:26:52 +0530

    Added PostGres SQL environment variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os # to read environment variables
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +10,7 @@ from resources.store import Store,StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False;
 app.secret_key = 'abhishek'
